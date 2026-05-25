d39c838c759a5886f7ce45bfc0b3c32af5272727
mohan-1698 <mohanvenky201@gmail.com>
2026-05-25 12:08:14 +0530

    first commit

---
+++ src/main/java/com/hackathon/hcl/Acres_Syntax_Squad/App.java
@@ -0,0 +1,13 @@
+package com.hackathon.hcl.Acres_Syntax_Squad;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
